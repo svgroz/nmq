e2ffb13f743adebfded14187f42be66ebf0e6c21
Simon Grozovsky <svgroz@outlook.com>
2022-06-29 22:37:01 +0300

    init

--- src/app/nmq.cpp
+++ src/app/nmq.cpp
@@ -1,6 +1,12 @@
-#include "nmq_server.pb.h"
+#include "nmq_server.grpc.pb.h"
+
+class Foo final : public proto::Api::Service
+{
+};
 
 int main()
 {
+  proto::SendMessageRequest req;
+  req.mutable_payload()->mutable_value();
   return 0;
 }
