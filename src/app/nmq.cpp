37a53121bd408a85889cefaf70de12169b3e9ee2
Simon Grozovsky <svgroz@outlook.com>
2022-06-30 20:32:03 +0300

    init

--- src/app/nmq.cpp
+++ src/app/nmq.cpp
@@ -2,11 +2,14 @@
 
 class Foo final : public proto::Api::Service
 {
+  public:
+    Foo();
+    virtual ~Foo();
+    virtual ::grpc::Status Send(::grpc::ServerContext* context, const ::proto::SendRequest* request, ::proto::SendResponse* response);
+    virtual ::grpc::Status Poll(::grpc::ServerContext* context, const ::proto::PollRequest* request, ::proto::PollResponse* response);
 };
 
 int main()
 {
-  proto::SendMessageRequest req;
-  req.mutable_payload()->mutable_value();
   return 0;
-}
+};
