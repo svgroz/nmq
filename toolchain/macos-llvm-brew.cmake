e00c55e53104a74df4fe5fc66efc74a432a08179
Simon Grozovsky <svgroz@outlook.com>
2022-07-08 23:58:39 +0300

    f

--- toolchain/macos-llvm-brew.cmake
+++ toolchain/macos-llvm-brew.cmake
@@ -2,7 +2,8 @@ set(CMAKE_C_COMPILER /usr/local/opt/llvm/bin/clang)
 set(CMAKE_CXX_COMPILER /usr/local/opt/llvm/bin/clang++)
 #set(CMAKE_CXX_CLANG_TIDY /usr/local/opt/llvm/bin/clang-tidy)
 set(CMAKE_CXX_FLAGS -I/usr/local/opt/llvm/include)
+set(CMAKE_PREFIX_PATH /usr/local/opt/llvm)
 
-link_directories(/usr/local/opt/llvm/lib)
+#link_directories(/usr/local/opt/llvm/lib)
 
 set(OPENSSL_ROOT_DIR /usr/local/opt/openssl)
