391eb39bc1e1b60fb49887ddcf9b78ff478d5278
Simon Grozovsky <svgroz@outlook.com>
2022-07-30 13:03:14 +0000

    some io fixes

--- core/src/nmq/core/index_log.cpp
+++ core/src/nmq/core/index_log.cpp
@@ -1,39 +1,35 @@
-#include "nmq/core/types.h"
 #include <array>
-#include <asm-generic/errno.h>
-#include <boost/numeric/conversion/cast.hpp>
-#include <cerrno>
-#include <cstddef>
 #include <cstdint>
 #include <cstring>
 #include <fstream>
 #include <memory>
+#include <tuple>
+#include <vector>
+
+#include <spdlog/spdlog.h>
+
 #include <nmq/core/exceptions.h>
 #include <nmq/core/index_log.h>
-#include <spdlog/spdlog.h>
-#include <tuple>
-#include <type_traits>
-#include <utility>
-#include <vector>
+#include <nmq/core/types.h>
 
 namespace nmq {
 
-constexpr std::int_fast64_t PAGE_SIZE =
+inline constexpr std::int_fast64_t PAGE_SIZE =
     // static_cast<std::int_fast64_t>(64 * 1024); // 64kb
     static_cast<std::int_fast64_t>((sizeof(IndexChunk) * 3) +
                                    1); // for test purposes only
-constexpr std::int_fast64_t CHUNKS_ON_PAGE = PAGE_SIZE / sizeof(IndexChunk);
-constexpr std::int_fast64_t CHUNKS_ON_PAGE_RAW_SIZE =
+inline constexpr std::int_fast64_t CHUNKS_ON_PAGE =
+    PAGE_SIZE / sizeof(IndexChunk);
+inline constexpr std::int_fast64_t CHUNKS_ON_PAGE_RAW_SIZE =
     CHUNKS_ON_PAGE * sizeof(IndexChunk);
-constexpr std::int_fast64_t PAGE_TILE_SIZE =
+inline constexpr std::int_fast64_t PAGE_TILE_SIZE =
     PAGE_SIZE - CHUNKS_ON_PAGE_RAW_SIZE;
-constexpr std::int_fast64_t NOT_FOUND = -1;
+inline constexpr std::int_fast64_t NOT_FOUND = -1;
 
 IndexLog::IndexLog(std::string &filename)
     : _filename(filename),
       _file(_filename, std::fstream::in | std::fstream::out |
-                           std::fstream::binary | std::fstream::app |
-                           std::fstream::ate) {
+                           std::fstream::binary | std::fstream::app) {
 
   if (_file.good()) {
     spdlog::info("file is opened: {}", _filename);
@@ -64,13 +60,13 @@ auto IndexLog::push_back(IndexChunk index_chunk) -> void {
 
 auto search_on_page(const IndexChunk *page, const std::size_t page_size,
                     const message_offset_t offset)
-    -> std::pair<int, std::int_fast64_t> {
+    -> std::tuple<std::int_fast32_t, std::int_fast64_t> {
   if (offset < page[0]._offset) {
-    return std::make_pair(-1, NOT_FOUND);
+    return std::make_tuple(-1, NOT_FOUND);
   }
 
   if (offset > page[page_size - 1]._offset) {
-    return std::make_pair(1, NOT_FOUND);
+    return std::make_tuple(1, NOT_FOUND);
   }
 
   std::size_t L = 0;
@@ -95,7 +91,7 @@ auto load_page(std::int_fast64_t page_index, char *buffer, std::fstream &file,
                std::int_fast64_t file_size) -> std::int_fast64_t {
   std::int_fast64_t current_page_start = page_index * PAGE_SIZE;
   std::int_fast64_t to_read = file_size - current_page_start > 0
-                                  ? PAGE_SIZE
+                                  ? PAGE_SIZE - 1
                                   : file_size - current_page_start;
   file.seekg(current_page_start);
   file.read(buffer, to_read);
@@ -114,14 +110,14 @@ auto find_page_with_offset(message_offset_t offset, std::fstream &file,
     std::int_fast64_t current_page_index = (left_index + right_index) / 2;
     std::int_fast64_t chunks_in_ptr =
         load_page(current_page_index, buffer, file, file_size);
-    auto search_result = search_on_page(index_chunk_ptr, chunks_in_ptr, offset);
-    if (search_result.first > 0) {
+    auto [found, index] =
+        search_on_page(index_chunk_ptr, chunks_in_ptr, offset);
+    if (found > 0) {
       left_index = current_page_index + 1;
-    } else if (search_result.first < 0) {
+    } else if (found < 0) {
       right_index = current_page_index - 1;
     } else {
-      return std::make_tuple(current_page_index, search_result.second,
-                             chunks_in_ptr);
+      return std::make_tuple(current_page_index, index, chunks_in_ptr);
     }
   } while (left_index <= right_index);
   return std::make_tuple(NOT_FOUND, NOT_FOUND, NOT_FOUND);
@@ -152,13 +148,18 @@ auto IndexLog::load(message_offset_t offset, std::size_t count)
       page_numer == pages_available - 1 ? chunks_on_page - chunk_index : count;
 
   result->reserve(reserve_size);
+  std::size_t result_size = 0;
 
   do {
     for (auto i = chunk_index; i < chunks_on_page; i++) {
       result->push_back(index_chunk_ptr[i]);
+      result_size++;
+      if (result_size == count) {
+        return result;
+      }
     }
 
-    if (page_numer == pages_available - 1 || result->size() == count) {
+    if (page_numer == pages_available - 1) {
       return result;
     }
 
--- test_core/src/nmq/core/index_log.cpp
+++ test_core/src/nmq/core/index_log.cpp
@@ -10,10 +10,13 @@ TEST(IndexLogTest, BasicHappyPath) {
       ("testPushBackHappyPath" + std::to_string(std::time(nullptr)));
 
   nmq::IndexLog log = nmq::IndexLog(tmp_directory);
-  for (auto i = 0; i < 99; i++) {
+  for (auto i = 0; i < 99; i = i + 3) {
     nmq::IndexChunk c = {._offset = i, ._position = i, ._size = 9};
     log.push_back(c);
   }
 
-  log.load(50, 10);
+  auto r1 =log.load(90, 10);
+  auto r2 =log.load(0, 10);
+  auto r3 =log.load(22, 10);
+
 }
